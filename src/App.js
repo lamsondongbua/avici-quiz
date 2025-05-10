960d1e49e6e264d4a7aced9c92bb491eec72cf78
lamsondongbua <dungxoi1872004@gmail.com>
2025-05-10 09:13:55 +0700

    2:02:58

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
+import Header from './components/Header/Header';
+import {Outlet} from 'react-router-dom';
 
-function App() {
+
+const App = () =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='app-container'>
+      <div className='header-container'>
+        <Header/>
+      </div>
+      <div className='main-container'>
+        <div className='sidenav-container'>
+        
+        </div>
+        <div className='app-content'>
+          {/* outlet là một component đặc biệt được dùng để hiển thị các route con - đặt outlet ở phần route con sẽ được render */}
+          <Outlet/>
+        </div>
+      </div>
+
+      
+    </div> 
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Admin from './components/Admin/Admin';
+import User from './components/User/User';
+import HomePage from './components/Home/HomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element = {<App/>}>
+        <Route index element= {<HomePage/>}/>
+        <Route path='/users' element = {<User/>}/>
+        <Route path='/admins' element = {<Admin/>}/>
+      </Route>
+    </Routes>
+  </BrowserRouter>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
