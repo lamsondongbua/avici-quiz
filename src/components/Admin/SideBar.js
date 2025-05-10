ade6868b9919495a5c9fb7e6abdb9a09951b277a
lamsondongbua <dungxoi1872004@gmail.com>
2025-05-10 11:40:43 +0700

    sang chapter 6

---
+++ src/components/Admin/SideBar.js
@@ -0,0 +1,98 @@
+import 'react-pro-sidebar/dist/css/styles.css';
+import { GiAtomicSlashes } from "react-icons/gi";
+import { GiSeaTurtle } from "react-icons/gi";
+import './SideBar.scss'
+import {
+    ProSidebar,
+    Menu,
+    MenuItem,
+    SubMenu,
+    SidebarHeader,
+    SidebarFooter,
+    SidebarContent,
+} from 'react-pro-sidebar';
+
+import { FaTachometerAlt, FaGem, FaList, FaGithub, FaRegLaughWink, FaHeart } from 'react-icons/fa';
+import sidebarBg from '../../assets/bg2.jpg';
+
+
+const SideBar = (props) =>{
+    const {image, collapsed, toggled, handleToggleSidebar} = props;
+    return (
+        <>
+            <ProSidebar
+                image={sidebarBg}
+                collapsed={collapsed}
+                toggled={toggled}
+                breakPoint="md"
+                onToggle={handleToggleSidebar}
+            >
+                <SidebarHeader>
+                    <div
+                        style={{
+                            padding: '24px',
+                            textTransform: 'uppercase',
+                            fontWeight: 'bold',
+                            fontSize: 14,
+                            letterSpacing: '1px',
+                            overflow: 'hidden',
+                            textOverflow: 'ellipsis',
+                            whiteSpace: 'nowrap',
+                        }}
+                    >
+                        <GiAtomicSlashes size={'3em'} color={"red"}  />
+                        <span style={{marginLeft: "5px"}}>AVICI QUIZ</span>
+                    </div>
+                </SidebarHeader>
+
+                <SidebarContent>
+                    <Menu iconShape="circle">
+                        <MenuItem
+                            icon={<FaTachometerAlt />}
+                            suffix={<span className="badge red">Main</span>}
+                        >
+                            Dashboard
+                        </MenuItem>
+                    </Menu>
+                    <Menu iconShape="circle">
+                        <SubMenu
+                            // suffix={<span className="badge yellow">3</span>}
+                            // icon={<FaRegLaughWink />}
+                            icon = {<FaGem/>}
+                            title="Features"
+                        >
+                            <MenuItem> Quản lý Users</MenuItem>
+                            <MenuItem> Quản lý Bài Quiz</MenuItem>
+                            <MenuItem> Quản lý Câu Hỏi</MenuItem>
+                        </SubMenu>
+                    </Menu>
+                </SidebarContent>
+
+                <SidebarFooter style={{ textAlign: 'center' }}>
+                    <div
+                        className="sidebar-btn-wrapper"
+                        style={{
+                            padding: '20px 24px',
+                        }}
+                    >
+                        <a
+                            href="https://www.facebook.com/tiendung.pham.1807?locale=vi_VN"
+                            target="_blank"
+                            className="sidebar-btn"
+                            rel="noopener noreferrer"
+                        >
+                            <GiSeaTurtle size={'2em'} color='#14e3fa' />
+                            {!collapsed &&(
+                                <span style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' }}>
+                                    AVICI FACEBOOK
+                                </span>
+                            )}
+                        </a>
+                    </div>
+                </SidebarFooter>
+            </ProSidebar>
+        </>
+    )
+}
+
+export default SideBar
