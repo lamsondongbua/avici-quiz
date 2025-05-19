30ebd280ee3f2f2d2384dbde412f2eaf88cc78e3
lamsondongbua <dungxoi1872004@gmail.com>
2025-05-19 17:01:59 +0700

    chap8_1:24:00

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiServices";
+import _ from 'lodash'
+import './DetailQuiz.scss'
 const DetailQuiz = (props) => {
     //lấy tham số trên URL
     const params = useParams();
@@ -12,12 +14,48 @@ const DetailQuiz = (props) => {
 
     const fetchQuestions = async() => {
         let response = await getDataQuiz(quizId);
-        console.log('chẹc qué', response)
+        // console.log('chẹc qué', response)
+        if (response && response.EC === 0){
+            //xử lí một câu hỏi có 3 đáp án nhưng bị lặp lại id của câu hỏi trong từng đáp án
+            let raw = response.DT;
+            let data = _.chain(raw)
+                .groupBy("id")
+                .map((value,key) =>{
+                    let answers = [];
+                    let questionDescription, image = null;
+                    value.forEach((item, index) =>{
+                        if (index === 0){
+                            questionDescription = item.description;
+                            image = item.image;
+                        }
+                        answers.push(item.answers);
+                        // console.log("item answers ",item.answers)
+                    });
+                    // console.log('value - key', value, key);
+                    return {questionId: key, answers : answers, questionDescription: questionDescription, image: image}
+                }
+                )
+                .value()
+            // console.log(data);
+        }       
     }
     
     return (
         <div className="detail-quiz-container">
-            DetailQuiz
+            <div className="left-content">
+                <div className="title">
+
+                </div>
+                <div className="q-body">
+                    <img/>
+                </div>
+                <div className="q-content">
+                    question content
+                </div>
+            </div>
+            <div className="right-content">
+                count down
+            </div>
         </div>
     )
 }
